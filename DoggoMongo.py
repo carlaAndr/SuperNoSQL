e5dc2782e06069e08d7465b280ef16b77dcb0ce0
carlaAndr <csosaper@itam.mx>
2023-11-21 18:46:48 -0600

    Datos a Mongo

---
+++ DoggoMongo.py
@@ -0,0 +1,12 @@
+
+import pandas 
+import requests
+import pymongo
+from pymongo import MongoClient
+import json
+
+live_5OtO3lqVASaXYYhkRjxgj7JV3rHrxrcA4pCJvG7SobwVUpUVuvw4Vp4iRrjblhQk
+
+https://api.thedogapi.com/v1/images/search?limit=100&api_key=live_5OtO3lqVASaXYYhkRjxgj7JV3rHrxrcA4pCJvG7SobwVUpUVuvw4Vp4iRrjblhQk
+
+images/id/analisis?api_key
