a7d41841fb6b6b0e25b245dacad1dd8b1013b020
SantiVillaRam7 <santiagovillaram7@gmail.com>
2023-11-28 15:46:05 -0600

    Ya carga lo de neo4j de forma correcta

--- updataneo.py
+++ updataneo.py
@@ -1,10 +1,16 @@
 from neo4j import GraphDatabase
+
 def main():
     # Neo4j connection parameters
     uri = "bolt://localhost:7687"
     username = "neo4j"
     password = "12345678"
+    
+    #column_list = df['column_name'].tolist()
+    import pandas as pd
+    column_list = pd.read_csv('https://raw.githubusercontent.com/carlaAndr/SuperNoSQL/main/csv/character.csv')['_id'].tolist()
 
+    # Example usage:
     # Function to load CSV data into Neo4j
     def load_csv(url, node_label):
         query = (
@@ -12,16 +18,40 @@ def main():
             f"CREATE (n:{node_label}) SET n = row"
         )
         return query
-
+    def setRel(character_id):
+        query = (
+            f"MATCH (character:Character {{_id: '{character_id}'}})"
+            f"MATCH (appearance:Appearance {{_id: '{character_id}'}})"
+            f"MATCH (image:Image {{_id: '{character_id}'}})"
+            f"MATCH (connections:Connections {{_id: '{character_id}'}})"
+            f"MATCH (biography:Biography {{_id: '{character_id}'}})"
+            f"MATCH (powerstats:Powerstats {{_id: '{character_id}'}})"
+            f"MATCH (work:Work {{_id: '{character_id}'}})"
+            f"MERGE (character)-[:HAS_POWERSTATS]->(powerstats)"
+            f"MERGE (character)-[:HAS_CONNECTIONS]->(connections)"
+            f"MERGE (character) -[:WORKS_IN]->(work)"
+            f"MERGE (character) -[:LOOKS_LIKE]->(image)"
+            f"MERGE (character) -[:HAS_APPEARANCE]->(appearance)"
+            f"MERGE (character) -[:HAS_BIOGRAPHY]->(biography)"
+        )
+        return query
     # Load CSV files into Neo4j
-    csv_file = 'https://raw.githubusercontent.com/carlaAndr/SuperNoSQL/main/superheroes.csv'
-    node_label = 'Superhero'
+    csv_files = ['https://raw.githubusercontent.com/carlaAndr/SuperNoSQL/main/csv/appearance.csv',
+                'https://raw.githubusercontent.com/carlaAndr/SuperNoSQL/main/csv/biography.csv',
+                'https://raw.githubusercontent.com/carlaAndr/SuperNoSQL/main/csv/character.csv',
+                'https://raw.githubusercontent.com/carlaAndr/SuperNoSQL/main/csv/connections.csv',
+                'https://raw.githubusercontent.com/carlaAndr/SuperNoSQL/main/csv/image.csv',
+                'https://raw.githubusercontent.com/carlaAndr/SuperNoSQL/main/csv/powerstats.csv',
+                'https://raw.githubusercontent.com/carlaAndr/SuperNoSQL/main/csv/work.csv']
+    node_labels = ['Appearance', 'Biography', 'Character', 'Connections', 'Image', 'Powerstats', 'Work']
 
     with GraphDatabase.driver(uri, auth=(username, password)) as driver:
-        #for csv_file, node_label in zip(csv_files, node_labels):
+        for csv_file, node_label in zip(csv_files, node_labels):
             with driver.session() as session:
                 query = load_csv(csv_file, node_label)
                 session.run(query)
+                for i in column_list:
+                    session.run(setRel(i))
                 
 if __name__ == "__main__":
     main()
